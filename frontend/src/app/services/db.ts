0e13b890161d65b65bd135041099ce2c830ab7a6
gyenesmate <gyenes.mate2002@gmail.com>
2026-07-29 23:20:41 +0200

    fix: secrets hidden

--- frontend/src/app/services/db.ts
+++ frontend/src/app/services/db.ts
@@ -4,19 +4,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
+import secrets from '../../../secrets.json';
 // Replace the values below with your project's credentials. You can
 // safely commit this file to version control; the only thing that
 // changes between environments is the values inside `firebaseConfig`.
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBAgub0vMaFhQRilSeMNiNwP7SjPp7vVik",
-  authDomain: "cryptowatcher-f7737.firebaseapp.com",
-  projectId: "cryptowatcher-f7737",
-  storageBucket: "cryptowatcher-f7737.firebasestorage.app",
-  messagingSenderId: "584896229688",
-  appId: "1:584896229688:web:4568a1a759bc6268bc11cd",
-  measurementId: "G-Z675TW4VXY"
-};
+const firebaseConfig = secrets.firebaseConfig;
 
 // initialize and export Firestore and Auth instances
 const app = initializeApp(firebaseConfig);
